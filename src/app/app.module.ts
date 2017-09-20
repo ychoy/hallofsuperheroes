490f0825578a84923116c7377f37397354b5fcd9
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-09-20 16:46:56 -0700

    register hero detail component and input module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { AppComponent } from './app.component';
+
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
@@ -9,8 +11,9 @@ import { AppComponent } from './app.component';
     FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
