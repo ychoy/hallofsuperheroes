96cf5bc9e6d25f913b5bbbb7b609185916bf0491
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-09-20 13:56:17 -0700

    show one hero with databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Hall of Superheroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Jessica Jones'
+  };
 }
+
